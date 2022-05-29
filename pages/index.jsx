a441c6694f38667f643e96a729b324752a77658b
AnLong98 <pedjaglavas98@gmail.com>
2022-05-29 18:33:22 +0200

    Added google domain verification

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Predrag Glavas</title>
+        <meta name="google-site-verification" content="900a7fag5_0syZn3-Gp3H8ETdYj15lDpmoMyGwuoOog" />
         <link rel="icon" href="/favicon-32x32.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin='true'></link>
