b1d0ebe0cdc4d44dec2e5ce346f897f5323cf1cf
AnLong98 <pedjaglavas98@gmail.com>
2022-10-04 21:49:01 +0200

    added favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,12 +7,12 @@ export default function Home() {
       <Head>
         <title>Predrag Glavas</title>
         <meta name="google-site-verification" content="900a7fag5_0syZn3-Gp3H8ETdYj15lDpmoMyGwuoOog" />
-        <link rel="icon" href="/favicon-32x32.png" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin='true'></link>
         <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet"></link>
       </Head>
-        <About />
+      <About />
     </div>
   )
 }
